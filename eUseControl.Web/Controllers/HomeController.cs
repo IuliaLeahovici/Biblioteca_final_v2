dfab64e9c7de661241d453ab9e50ec4ea2654d26
Iulia Leahovici <leahiulia22@gmail.com>
2025-05-21 16:21:55 +0300

    am adaugat home cotroller,base controller si interfata ISession

---
+++ eUseControl.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void GetHeaderData()
+        {
+            SessionStatus();
+            var apiCookie = System.Web.HttpContext.Current.Request.Cookies["X-KEY"];
+            string userStatus = (string)System.Web.HttpContext.Current.Session["LoginStatus"];
+            if (userStatus != "guest")
+            {
+                var profile = _session.GetUserByCookie(apiCookie.Value);
+                ViewBag.user = profile;
+            }
+            ViewBag.userStatus = userStatus;
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            GetHeaderData();
+            var bestsellerBooks = _session.GetBooksList().OrderByDescending(f => f.Bought).Take(4);
+            var cheapBooks = _session.GetBooksList().OrderBy(f => f.Price).Take(6);
+            ViewBag.bestsellerBooks = bestsellerBooks;
+            ViewBag.cheapBooks = cheapBooks;
+            return View();
+        }
+    }
+}
