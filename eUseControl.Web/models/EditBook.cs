2d5af5b890c1952b7eee8c8538938120811572c1
Lera <Lera@DESKTOP-4NRDPOR>
2025-05-24 03:02:01 +0300

    am adăugat modele pentru adăugarea și editarea cărților și a utilizatorilor

---
+++ eUseControl.Web/models/EditBook.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eUseControl.Web.Models
+{
+    public class EditBook
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string Edit { get; set; }
+
+    }
+}
